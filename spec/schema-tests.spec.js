0677af4020d8aee0e322b78932662a2fd59bd93f
Evgeny Poberezkin <e.poberezkin@me.com>
2016-12-28 21:23:54 +0000

    feat: keyword prohibited

--- spec/schema-tests.spec.js
+++ spec/schema-tests.spec.js
@@ -7,7 +7,7 @@ var defineKeywords = require('..');
 var ajvs = [
   // defineKeywords(getAjv(),
   //   ['switch', 'patternRequired', 'formatMinimum', 'formatMaximum']),
-  defineKeywords(getAjv(), ['if', 'deepRequired', 'deepProperties']),
+  defineKeywords(getAjv(), ['if', 'prohibited', 'deepRequired', 'deepProperties']),
   defineKeywords(getAjv()),
   defineKeywords(getAjv(true))
 ];
