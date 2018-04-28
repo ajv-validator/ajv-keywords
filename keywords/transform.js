e9789e300097c14c9945989fef1f007a2fb43411
Evgeny Poberezkin <e.poberezkin@me.com>
2018-04-28 10:13:04 +0100

    remove ignored "schema" parameter from definition

--- keywords/transform.js
+++ keywords/transform.js
@@ -5,7 +5,6 @@ module.exports = function defFunc (ajv) {
     type: 'string',
     errors: false,
     modifying: true,
-    schema: false,
     valid: true,
     compile: function (schema, parentSchema) {
 
