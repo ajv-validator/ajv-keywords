a362cc8e5fc4f22b3708e44e1f6941a6c7ede016
jsone-studios <jsone-studios@users.noreply.github.com>
2020-03-23 18:08:36 +0100

    Remove explicit list of keywords

--- ajv-keywords.d.ts
+++ ajv-keywords.d.ts
@@ -1,28 +1,7 @@
 declare module 'ajv-keywords' {
   import { Ajv } from 'ajv';
-  
-  type AdditionalKeywords =
-    | 'typeof'
-    | 'instanceof'
-    | 'range'
-    | 'regexp'
-    | 'formatMinimum'
-    | 'formatMaximum'
-    | 'transform'
-    | 'uniqueItemProperties'
-    | 'allRequired'
-    | 'anyRequired'
-    | 'oneRequired'
-    | 'patternRequired'
-    | 'prohibited'
-    | 'deepProperties'
-    | 'deepRequired'
-    | 'switch'
-    | 'select'
-    | 'dynamicDefaults'
-    ;
 
-  function keywords(ajv: Ajv, include?: AdditionalKeywords | AdditionalKeywords[]): Ajv;
+  function keywords(ajv: Ajv, include?: string | string[]): Ajv;
 
   export = keywords;
 }
