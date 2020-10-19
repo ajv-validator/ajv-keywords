a4bb9e107003c0af4e90cd42ccf97dd9233719c1
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-10-19 12:37:08 +0100

    typeof test to typescript

--- spec/typeof.spec.ts
+++ spec/typeof.spec.ts
@@ -1,21 +1,21 @@
-"use strict"
+import Ajv from "ajv"
+import typeofPlugin from "../dist/keywords/typeof"
+import ajvKeywordsPlugin from "../dist"
+import chai from "chai"
 
-const Ajv = require("ajv")
-// var ajvPack = require('ajv-pack');
-const defFunc = require("../dist/keywords/typeof")
-const defineKeywords = require("../dist")
-const should = require("chai").should()
+// const ajvPack = require('ajv-pack');
+const should = chai.should()
 
 describe('keyword "typeof"', () => {
   const ajvs = [
-    defFunc(new Ajv()),
-    defineKeywords(new Ajv(), "typeof"),
-    defineKeywords(new Ajv()),
+    typeofPlugin(new Ajv()),
+    ajvKeywordsPlugin(new Ajv(), "typeof"),
+    ajvKeywordsPlugin(new Ajv()),
     // defFunc(ajvPack.instance(new Ajv({sourceCode: true})))
   ]
 
   ajvs.forEach((ajv, i) => {
-    it("should validate value types #" + i, () => {
+    it(`should validate value types #${i}`, () => {
       ajv.validate({typeof: "undefined"}, undefined).should.equal(true)
       ajv.validate({typeof: "undefined"}, null).should.equal(false)
       ajv.validate({typeof: "undefined"}, "foo").should.equal(false)
@@ -28,13 +28,13 @@ describe('keyword "typeof"', () => {
       ajv.validate({typeof: "symbol"}, {}).should.equal(false)
     })
 
-    it("should validate multiple types #" + i, () => {
+    it(`should validate multiple types #${i}`, () => {
       ajv.validate({typeof: ["string", "function"]}, "foo").should.equal(true)
       ajv.validate({typeof: ["string", "function"]}, () => {}).should.equal(true)
       ajv.validate({typeof: ["string", "function"]}, {}).should.equal(false)
     })
 
-    it("should throw when unknown type is passed #" + i, () => {
+    it(`should throw when unknown type is passed #${i}`, () => {
       should.throw(() => {
         ajv.compile({typeof: "unknownType"})
       })
@@ -44,7 +44,7 @@ describe('keyword "typeof"', () => {
       })
     })
 
-    it("should throw when not string or array is passed #" + i, () => {
+    it(`should throw when not string or array is passed #${i}`, () => {
       should.throw(() => {
         ajv.compile({typeof: 1})
       })
