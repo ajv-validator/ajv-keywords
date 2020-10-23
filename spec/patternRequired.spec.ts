f60906224fd9c07141c45da140193a3a3021f39c
Evgeny Poberezkin <2769109+epoberezkin@users.noreply.github.com>
2020-10-23 18:55:45 +0100

    test: patternRequired

--- spec/patternRequired.spec.ts
+++ spec/patternRequired.spec.ts
@@ -5,14 +5,11 @@ import chai from "chai"
 
 chai.should()
 
-// const Ajv = require("ajv")
-// const defFunc = require("../dist/keywords/patternRequired")
-// const defineKeywords = require("../dist")
-// require("chai").should()
-
-describe.skip('keywords "patternRequired"', () => {
-  const ajvs = getAjvInstances("instanceof", patternRequiredDef, patternRequired, {allErrors: true})
-  const ajvsOP = getAjvInstances("instanceof", patternRequiredDef, patternRequired, {
+describe('keywords "patternRequired"', () => {
+  const ajvs = getAjvInstances("patternRequired", patternRequiredDef, patternRequired, {
+    allErrors: true,
+  })
+  const ajvsOP = getAjvInstances("patternRequired", patternRequiredDef, patternRequired, {
     allErrors: true,
     ownProperties: true,
   })
@@ -22,20 +19,15 @@ describe.skip('keywords "patternRequired"', () => {
       const ajvOP = ajvsOP[i]
       const schema = {type: "object", patternRequired: ["f.*o"]}
 
-      class BazThing {
-        foooo = false
-        fooooooo = 42.31
-      }
+      const baz = {foooo: false, fooooooo: 42.31}
 
-      class FooThing extends BazThing {
-        bar?: number
+      type Constructor = new () => any
 
-        constructor(bar?: number) {
-          super()
-          if (bar) this.bar = bar
-        }
+      function FooThing(this: any): any {
+        this.bar = 123
       }
-      const object = new FooThing(123)
+      FooThing.prototype = baz
+      const object = new ((FooThing as unknown) as Constructor)()
 
       ajv.validate(schema, object).should.equal(true)
       ajvOP.validate(schema, object).should.equal(false)
